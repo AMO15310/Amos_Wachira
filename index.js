cdb62bc20ce4e6df1ac397d4d4a794fd54474b6d
AMO15310 <wachiraamos402@gmail.com>
2022-10-31 21:13:30 +0300

    Upload changes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const button1 = document.querySelector(".b1");
+const button2 = document.querySelector(".b2");
+const button3 = document.querySelector(".b3");
+const button4 = document.querySelector(".b4");
+const body = document.querySelector("body");
+
 const typedtextelement = document.querySelector(".typed-text");
 const breaking = `${"<br>"}`;
 const paragraph = [
@@ -9,14 +15,85 @@ let charindex = 0;
 let paragraphIndex = 0;
 
 const type = () => {
-  console.log(paragraph.length);
   if (charindex < paragraph[paragraphIndex].length) {
     typedtextelement.textContent += paragraph[paragraphIndex].charAt(charindex);
 
     charindex++;
 
-    // paragraphIndex++;
     setTimeout(type, 150);
   }
 };
-type();
+setTimeout(type, 4000);
+const images = [];
+images.push(
+  "../images/mountain.jpg",
+  "../images/skies.jpg",
+  "../images/mountgreen.jpg",
+  "../images/image2.jpg"
+);
+button1.addEventListener("click", () => {
+  body.style.backgroundImage = `url(${images[0]})`;
+  if (body.style.backgroundImage == `url("../images/mountain.jpg")`) {
+    button1.style.backgroundColor = `aqua`;
+    button2.style.backgroundColor = `white`;
+    button3.style.backgroundColor = `white`;
+    button4.style.backgroundColor = `white`;
+  }
+});
+button2.addEventListener("click", () => {
+  body.style.backgroundImage = `url(${images[1]})`;
+  if (body.style.backgroundImage == `url("../images/skies.jpg")`) {
+    button2.style.backgroundColor = `aqua`;
+    button1.style.backgroundColor = `white`;
+    button3.style.backgroundColor = `white`;
+    button4.style.backgroundColor = `white`;
+  }
+});
+button3.addEventListener("click", () => {
+  body.style.backgroundImage = `url(${images[2]})`;
+  if (body.style.backgroundImage == `url("../images/mountgreen.jpg")`) {
+    button3.style.backgroundColor = `aqua`;
+    button2.style.backgroundColor = `white`;
+    button1.style.backgroundColor = `white`;
+    button4.style.backgroundColor = `white`;
+  }
+});
+button4.addEventListener("click", () => {
+  body.style.backgroundImage = `url(${images[3]})`;
+  if (body.style.backgroundImage == `url("../images/image2.jpg")`) {
+    button4.style.backgroundColor = `aqua`;
+    button3.style.backgroundColor = `white`;
+    button2.style.backgroundColor = `white`;
+    button1.style.backgroundColor = `white`;
+  }
+});
+
+setInterval(() => {
+  body.style.backgroundImage = `url(${
+    images[Math.floor(Math.random() * images.length)]
+  }`;
+  if (body.style.backgroundImage == `url("../images/mountain.jpg")`) {
+    button1.style.backgroundColor = `aqua`;
+    button2.style.backgroundColor = `white`;
+    button3.style.backgroundColor = `white`;
+    button4.style.backgroundColor = `white`;
+  }
+  if (body.style.backgroundImage == `url("../images/skies.jpg")`) {
+    button2.style.backgroundColor = `aqua`;
+    button1.style.backgroundColor = `white`;
+    button3.style.backgroundColor = `white`;
+    button4.style.backgroundColor = `white`;
+  }
+  if (body.style.backgroundImage == `url("../images/mountgreen.jpg")`) {
+    button3.style.backgroundColor = `aqua`;
+    button2.style.backgroundColor = `white`;
+    button1.style.backgroundColor = `white`;
+    button4.style.backgroundColor = `white`;
+  }
+  if (body.style.backgroundImage == `url("../images/image2.jpg")`) {
+    button4.style.backgroundColor = `aqua`;
+    button3.style.backgroundColor = `white`;
+    button2.style.backgroundColor = `white`;
+    button1.style.backgroundColor = `white`;
+  }
+}, 5000);
